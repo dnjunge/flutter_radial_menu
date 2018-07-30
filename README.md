6bbf29d5ed6ae195471acf66af3ffabbef5b575b
dnjunge <dnjunge001@gmail.com>
2018-07-30 11:57:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_radial_menu
+
+A new Flutter application.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.cancel,
+          ),
+          onPressed: (){},
+        ),
+        title: new Text(''),
+      ),
+      body: new Center(
+      ),
+    );
+  }
+}
